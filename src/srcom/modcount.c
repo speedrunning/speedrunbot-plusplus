63d3231b1c3030a892951e1ce7b7e8c0dc629e6e
Mango0x45 <thomasvoss@live.com>
2021-02-21 18:01:36 +0100

    Rewrite +modcount in C

---
+++ src/srcom/modcount.c
@@ -0,0 +1,37 @@
+/*
+ * This program gets the number of games and series that a given player
+ * (argv[1]) is a moderator of.
+ */
+
+/* TODO: Calculate games and series concurrently */
+
+#include <stdio.h>
+
+#include "utils.h"
+
+int main(int UNUSED(argc), char **argv)
+{
+	char *uid = get_uid(argv[1]);
+
+	string_t games, series;
+	init_string(&games);
+	init_string(&series);
+
+	/*
+	 * Setting _bulk to `yes` increases the game limit from 200 to 1000. But
+	 * who in their right mind would moderate >200 games anyways? Only one
+	 * that comes to mind is April kek.
+	 */
+	static char uri[URIBUF];
+	snprintf(uri, URIBUF, API "/games?moderator=%s&_bulk=yes&max=1000",
+	         uid);
+	get_req(uri, &games);
+	snprintf(uri, URIBUF, API "/series?moderator=%s&max=200", uid);
+	get_req(uri, &series);
+
+	const unsigned int gcount = substr("\"id\":", games.ptr, games.len);
+	const unsigned int scount = substr("\"id\":", series.ptr, series.len);
+
+	printf("%u %u %u\n", gcount, scount, gcount + scount);
+	return EXIT_SUCCESS;
+}
