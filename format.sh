d59cd55ca8de37689d6b836eeed4e1bb6faf3d5f
Mango0x45 <thomasvoss@live.com>
2021-02-21 15:18:54 +0100

    Add C formatting

--- format.sh
+++ format.sh
@@ -3,5 +3,9 @@
 for FILE in $(find ./* | grep '\.py$'); do
     isort "$FILE" >/dev/null 2>&1
     black "$FILE" >/dev/null 2>&1
-    echo "Formatted $FILE"
+    echo "Formatting $FILE"
+done
+
+for FILE in $(find ./* | grep '\.[ch]$'); do
+    clang-format -i --verbose --sort-includes -style=file "$FILE"
 done
