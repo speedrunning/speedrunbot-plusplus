a8eeb516d8727757cefe942a16205557ded8eb95
Mango0x45 <thomasvoss@live.com>
2021-02-21 15:24:42 +0100

    Tabs pls

--- format.sh
+++ format.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 for FILE in $(find ./* | grep '\.py$'); do
-    isort "$FILE" >/dev/null 2>&1
-    black "$FILE" >/dev/null 2>&1
-    echo "Formatting $FILE"
+	isort "$FILE" >/dev/null 2>&1
+	black "$FILE" >/dev/null 2>&1
+	echo "Formatting $FILE"
 done
 
 for FILE in $(find ./* | grep '\.[ch]$'); do
-    clang-format -i --verbose --sort-includes -style=file "$FILE"
+	clang-format -i --verbose --sort-includes -style=file "$FILE"
 done
