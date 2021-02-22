094592b9cc1a41f95b0d7b7ff8d5b7f2bb47df77
Mango0x45 <thomasvoss@live.com>
2021-02-22 13:28:36 +0100

    Properly align times when displayed on the screen

--- src/srcom/leaderboard.py
+++ src/srcom/leaderboard.py
@@ -10,6 +10,15 @@ from sys import argv, exit
 import requests
 from utils import *
 
+
+def pad(time: str) -> str:
+    """
+    Pad a time with blank spaces if it doesnt contain milliseconds for output
+    formatting.
+    """
+    return f"{time}    " if "." not in time else time
+
+
 # Get the game ID and name
 r: dict = requests.get(f"{API}/games?abbreviation={argv[1]}").json()
 GID: str = r["data"][0]["id"]
@@ -44,7 +53,7 @@ r = requests.get(f"{API}/leaderboards/{GID}/category/{cid}?top=10").json()
 rows: list[list[str]] = [
     [
         str(run["place"]),
-        ptime(run["run"]["times"]["primary_t"]),
+        pad(ptime(run["run"]["times"]["primary_t"])),
         username(run["run"]["players"][0]["id"]),
     ]
     for run in r["data"]["runs"]
@@ -53,16 +62,12 @@ rows: list[list[str]] = [
 # Length of the longest run time, used for output padding
 MAXLEN: int = max([len(i[1]) for i in rows])
 
-# TODO: Align run times like so:
-#   59:03.364
-#   59:24
-# 1:00:21.349
 print(
     f"{GAME} - {CAT}"
     + "\n"
     + "\n".join(
         [
-            f"{row[0].rjust(2).ljust(3)} {row[1].ljust(MAXLEN + 1)} {row[2]}"
+            f"{row[0].rjust(2).ljust(3)} {row[1].rjust(MAXLEN).ljust(MAXLEN + 1)} {row[2]}"
             for row in rows
         ]
     )
