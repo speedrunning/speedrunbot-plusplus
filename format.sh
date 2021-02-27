baa7f030a72bcad5e1f1340f21d93e77e7bfeea1
Mango0x45 <thomasvoss@live.com>
2021-02-27 13:43:42 +0100

    Better format script

--- format.sh
+++ format.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-for FILE in $(find ./* | grep '\.py$'); do
-	isort "$FILE" >/dev/null 2>&1
-	black -l 80 "$FILE" >/dev/null 2>&1
-	echo "Formatting $FILE"
+shopt -s globstar nullglob
+
+SCR_PATH=$(cd "$(dirname "$0")" && pwd)
+
+for FILE in "$SCR_PATH"/**/*.py; do
+    isort "$FILE" >/dev/null 2>&1
+    black -l 80 "$FILE" >/dev/null 2>&1
+    echo "Formatting $FILE"
 done
 
-for FILE in $(find ./* | grep '\.[ch]$'); do
-	clang-format -i --verbose --sort-includes -style=file "$FILE"
+for FILE in "$SCR_PATH"**/*.[ch]; do
+    clang-format -i --verbose --sort-includes -style=file "$FILE"
 done
