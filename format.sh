2eb1251a2f71f1063c183bd07313eb3f291f6590
Mango0x45 <thomasvoss@live.com>
2021-03-18 13:36:43 +0100

    Nicer formatting script

--- format.sh
+++ format.sh
@@ -6,12 +6,21 @@ shopt -s globstar nullglob
 
 SCR_PATH=$(cd "$(dirname "$0")" && pwd)
 
-for FILE in "$SCR_PATH"/**/*.py; do
-	isort "$FILE" &>/dev/null
-	python3.9 -m black -l 80 "$FILE" &>/dev/null
-	echo "Formatting $FILE"
-done
-
-for FILE in "$SCR_PATH"**/*.[ch]; do
-	clang-format -i --verbose --sort-includes -style=file "$FILE"
+for FILE in "$SCR_PATH"/**/*; do
+	case $FILE in
+	*.[ch])
+		clang-format -i --verbose --sort-includes -style=file "$FILE"
+		;;
+	*.py)
+		isort "$FILE" &>/dev/null
+		python3.9 -m black -l 80 "$FILE" &>/dev/null
+		echo "Formatting $FILE"
+		;;
+	*.sh)
+		echo "Formatting $FILE"
+		unexpand -t 8 --first-only "$FILE" >temp
+		mv temp "$FILE"
+		chmod +x "$FILE"
+		;;
+	esac
 done
