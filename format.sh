6c5fdc234453a8989c5522299189cdabee9c4046
Mango0x45 <thomasvoss@live.com>
2021-03-21 16:26:57 +0100

    Check for formatting programs and install them if they aren't already

--- format.sh
+++ format.sh
@@ -2,6 +2,42 @@
 
 # Format all the source files.
 
+if ! command -v black &>/dev/null; then
+	echo Error: Black was not found. This script will break when \
+		formatting python files.
+	printf "Do you want to install black? [y/N]: "
+	read -r C
+	if test "$C" = "y" || test "$C" = "Y"; then
+		python3 -m pip install black
+	fi
+fi
+if ! command -v isort &>/dev/null; then
+	echo Error: isort was not found. This script will break when \
+		formatting python files.
+	printf "Do you want to install isort? [y/N]: "
+	read -r C
+	if test "$C" = "y" || test "$C" = "Y"; then
+		python3 -m pip install isort
+	fi
+fi
+if ! command -v clang-format &>/dev/null; then
+	echo Error: clang-format was not found. This script will break when \
+		formatting C files.
+	printf "Do you want to install clang-format? (Debian only) [y/N]: "
+	read -r C
+	if test "$C" = "y" || test "$C" = "Y"; then
+		if command -v sudo >/dev/null 2>&1; then
+			SU="sudo"
+		elif command -v doas >/dev/null 2>&1; then
+			SU="doas"
+		else
+			printf "Command to gain superuser privileges (typically sudo or doas): "
+			read -r SU
+		fi
+		yes | $SU apt install clang-format
+	fi
+fi
+
 shopt -s globstar nullglob
 
 SCR_PATH=$(cd "$(dirname "$0")" && pwd)
@@ -12,7 +48,7 @@ for FILE in "$SCR_PATH"/**/*; do
 		clang-format -i --verbose --sort-includes -style=file "$FILE"
 		;;
 	*.py)
-		echo "Formatting $FILE"
+		echo Formatting "$FILE"
 		isort "$FILE" &>/dev/null
 		python3.9 -m black -l 80 "$FILE" &>/dev/null
 		unexpand -t 4 --first-only "$FILE" >temp
@@ -25,7 +61,7 @@ for FILE in "$SCR_PATH"/**/*; do
 		test $EFLAG -eq 1 && chmod +x "$FILE"
 		;;
 	*.sh)
-		echo "Formatting $FILE"
+		echo Formatting "$FILE"
 		unexpand -t 8 --first-only "$FILE" >temp
 		mv temp "$FILE"
 		chmod +x "$FILE"
