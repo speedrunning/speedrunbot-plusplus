7a7c133878f0fe5f2fd3eed61349023751f8a928
Mango0x45 <thomasvoss@live.com>
2021-03-21 14:53:42 +0100

    Turn spaces in python files to tabs

--- format.sh
+++ format.sh
@@ -12,9 +12,17 @@ for FILE in "$SCR_PATH"/**/*; do
 		clang-format -i --verbose --sort-includes -style=file "$FILE"
 		;;
 	*.py)
+		echo "Formatting $FILE"
 		isort "$FILE" &>/dev/null
 		python3.9 -m black -l 80 "$FILE" &>/dev/null
-		echo "Formatting $FILE"
+		unexpand -t 4 --first-only "$FILE" >temp
+		if test -x "$FILE"; then
+			EFLAG=1
+		else
+			EFLAG=0
+		fi
+		mv temp "$FILE"
+		test $EFLAG -eq 1 && chmod +x "$FILE"
 		;;
 	*.sh)
 		echo "Formatting $FILE"
