da287227cb1ac3db04fa75f3f4bc4785a7e094a9
Thomas Voss <57815710+Mango0x45@users.noreply.github.com>
2021-03-21 14:58:53 +0100

    Don't print a title in output

--- src/srcom/queue.py
+++ src/srcom/queue.py
@@ -17,15 +17,15 @@ from utils import *
 
 
 async def queue(GID: str) -> int:
-        """
-        Get the number of runs in the queue of the game with ID `GID`. Since
-        each request can only return at most 200 runs, 5 requests of 200 runs
-        are done in parallel. If none of the requests have less than 200 runs,
-        additional series of 5 requests are made until the condition is met.
-        """
-        runcount: int = 0
-        
-        for offstart in count(0, 1000):
+	"""
+	Get the number of runs in the queue of the game with ID `GID`. Since
+	each request can only return at most 200 runs, 5 requests of 200 runs
+	are done in parallel. If none of the requests have less than 200 runs,
+	additional series of 5 requests are made until the condition is met.
+	"""
+	runcount: int = 0
+
+	for offstart in count(0, 1000):
 		with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 			loop: AbstractEventLoop = asyncio.get_event_loop()
 			futures: Iterable[Awaitable] = (
@@ -38,26 +38,26 @@ async def queue(GID: str) -> int:
 			)
 			for response in await asyncio.gather(*futures):
 				r: dict = response.json()
-                                size: int = len(r["data"])
-                                runcount += size
-                                if size < 200:
-                                        return runcount
+	                        size: int = len(r["data"])
+	                        runcount += size
+	                        if size < 200:
+	                                return runcount
 
 
 def main() -> int:
-        try:
-                GAME, GID: str = game(argv[1])
-        except GameError as e:
-                print(f"Error: {e}", file=stderr)
-                return EXIT_FAILURE
-        
-        LOOP: AbstractEventLoop = asyncio.get_event_loop()
-        LENGTH: int = LOOP.run_until_complete(queue(GID))
-        
-        print(f"Queue: {GAME}\nLength: {LENGTH}")
-        return EXIT_SUCCESS
+	try:
+		GID: str = game(argv[1])[1]
+	except GameError as e:
+		print(f"Error: {e}", file=stderr)
+		return EXIT_FAILURE
+
+	LOOP: AbstractEventLoop = asyncio.get_event_loop()
+	LENGTH: int = LOOP.run_until_complete(queue(GID))
+
+	print(f"Queue Length: {LENGTH}")
+	return EXIT_SUCCESS
 
 
 if __name__ == "__main__":
-        RET: int = main()
-        exit(RET)
+	RET: int = main()
+	exit(RET)
