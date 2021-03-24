a877d5416479e0c7ccb7ce4a4c72056dcb209b84
Mango0x45 <thomasvoss@live.com>
2021-03-24 18:39:11 +0100

    Make the script function from any calling directory

--- setup.sh
+++ setup.sh
@@ -88,6 +88,6 @@ yes | $SU apt install libjansson-dev libcurl4-openssl-dev >/dev/null 2>&1
 
 # Run the Makefiles.
 echo Building executables
-for f in $(find . | grep 'Makefile'); do
+for f in $(find "$SCR_PATH" | grep 'Makefile'); do
 	make -C "$(dirname "$f")" >/dev/null 2>&1
 done
