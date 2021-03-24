69dfdad3331f6b08e9308dde57843613dbe69172
Mango0x45 <thomasvoss@live.com>
2021-03-24 16:40:52 +0100

    Move defines into `defines.h`

--- include/srcom/utils.h
+++ include/srcom/utils.h
@@ -7,19 +7,6 @@
 #define UIDBUF 16
 #define URIBUF 128
 
-/* Supress unused parameter warnings */
-#ifdef __GNUC__
-#	define UNUSED(x) UNUSED_##x __attribute__((__unused__))
-#else
-#	define UNUSED(x) UNUSED_##x
-#endif
-
-/* For when I haven't included <stdlib.h> */
-#ifndef EXIT_SUCCESS
-#	define EXIT_SUCCESS 0
-#	define EXIT_FAILURE 1
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
