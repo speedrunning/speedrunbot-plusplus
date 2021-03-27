f95bb99184ba75f52f643acae5ca415765447522
Mango0x45 <thomasvoss@live.com>
2021-03-27 12:19:52 +0100

    Make execv asyncronous and add a process limit

--- src/bot.py
+++ src/bot.py
@@ -1,19 +1,62 @@
+import asyncio
 import os
 from datetime import datetime
 from pathlib import Path
-from subprocess import CompletedProcess, run
 from sys import stderr
-from typing import Iterable
+from typing import Iterable, Union
 
 import discord
 from discord.ext import commands
+from discord.ext.commands.context import Context
 from discord.message import Message
+
+TITLE: str
+DESC: str
 
 PREFIX: Path = Path(__file__).parent
 ROOT_DIR: str = f"{PREFIX}/.."
 EXTENSIONS: Iterable[str] = (
 	f"cogs.{f[:-3]}" for f in os.listdir(f"{PREFIX}/cogs") if f.endswith(".py")
 )
+
+curr_procs: int = 0
+MAX_PROCS: int = 15
+
+
+async def execv(
+	ctx: Context, PROG: str, *ARGV: tuple[str, ...], TITLE: Union[str, None] = None
+) -> None:
+	"""
+	Run a program as a subprocess and return its output + exit code.
+	"""
+	global curr_procs, MAX_PROCS
+	if curr_procs == MAX_PROCS:
+		await ctx.send("Too many processes running. Try again in a moment.")
+		return
+	curr_procs += 1
+
+	ARGS: str = " ".join(f'"{arg}"' for arg in tuple(filter(lambda x: x, ARGV)))
+	RET = await asyncio.create_subprocess_shell(
+		f"{PREFIX}/{PROG} {ARGS}",
+		stdout=asyncio.subprocess.PIPE,
+		stderr=asyncio.subprocess.PIPE,
+	)
+	STDOUT, STDERR = await RET.communicate()
+
+	if RET.returncode != 0:
+		await ctx.send(STDERR.decode())
+		return
+
+	if TITLE is not None:
+		embed = discord.Embed(
+			title=f"{TITLE}: {ARGV[0]}", description=STDOUT.decode()
+		)
+	else:
+		TITLE, DESC = STDOUT.decode().split("\n", 1)
+		embed = discord.Embed(title=TITLE, description=DESC)
+	await ctx.send(embed=embed)
+
+	curr_procs -= 1
 
 
 class SRBpp(commands.Bot):
@@ -35,16 +78,6 @@ class SRBpp(commands.Bot):
 
 		with open(f"{ROOT_DIR}/token", "r") as f:
 			self.token = f.read().strip()
-
-	def execv(_, PROG: str, *ARGS: tuple[str, ...]) -> CompletedProcess:
-		"""
-		Run a program as a subprocess and return its output + exit code.
-		"""
-		return run(
-			(f"{PREFIX}/{PROG}",) + tuple(filter(lambda x: x, ARGS)),
-			capture_output=True,
-			text=True,
-		)
 
 	async def on_ready(self) -> None:
 		"""
