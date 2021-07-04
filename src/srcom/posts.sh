9a0782c6a4aa8eb1119e711116cace17c8147d27
Mango0x45 <thomasvoss@live.com>
2021-07-04 22:14:17 +0200

    Use curls range features to reduce the number of processes spawned

--- src/srcom/posts.sh
+++ src/srcom/posts.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 die () {
-	echo "$1" >&2
+	>&2 echo "$1"
 	exit 1
 }
 
 download () (
-	curl "https://www.speedrun.com/$1/allposts/$2" 2>/dev/null |
+	2>/dev/null curl "https://www.speedrun.com/$1/allposts/[$2-$(( $2 + REQS_PER_CORE - 1 ))]" |
 		sed -En "s|\s+<p class=\"mb-1\">Forum: <a href='/([^/]+)/forum'>.*</a></p>|\1|p" >>/tmp/$3
 )
 
@@ -16,7 +16,7 @@ Example: `+posts AnInternetTroll`'
 set -e
 
 # In the sed(1) command we branch to `:quit` the moment we match, if not it will match twice.
-MAX=$(curl "https://www.speedrun.com/$1/allposts" 2>/dev/null | sed -En '
+MAX=$(2>/dev/null curl "https://www.speedrun.com/$1/allposts" | sed -En '
 b sub
 :quit
 q
@@ -27,13 +27,15 @@ t quit')
 # TODO: Add better error messages maybe?
 [ -z $MAX ] && die "User '$1' is either not real, or has no posts. (Or the site is down)"
 
+REQS_PER_CORE=$(( MAX / 4 ))
+
 trap "rm -f /tmp/$$" EXIT
 
-i=0
+i=1
 until [ $i -gt $MAX ]; do
 	for _ in 1 2 3 4; do
-		: $(( i += 1 ))
 		download "$1" $i $$ &
+		: $(( i += REQS_PER_CORE ))
 	done
 	wait
 done
@@ -49,7 +51,7 @@ while read LINE; do
 	esac
 done </tmp/$$
 
-TOTAL=$(curl "https://www.speedrun.com/user/$1/info" 2>/dev/null |
+TOTAL=$(2>/dev/null curl "https://www.speedrun.com/user/$1/info" |
 	sed -En 's/.*Posts:[^[:digit:]]+([0-9]+).*/\1/p')
 
 echo "Site Forums: $SITE
